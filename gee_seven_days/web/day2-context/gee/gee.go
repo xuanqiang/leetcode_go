839cea69659c8ada24003bb5ce56b372cc2b505e
许安强先森 <837536729@qq.com>
2020-04-25 16:15:29 +0800

    七天手写web框架第二天

--- gee_seven_days/web/day2-context/gee/gee.go
+++ gee_seven_days/web/day2-context/gee/gee.go
@@ -1,28 +1,26 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // HandlerFunc defines the request handler used by gee
-type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+type HandlerFunc func(ctx *Context)
 
 // Engine implement the interface of ServeHTTP
 // 只要传入任何实现了 ServerHTTP 接口的实例，
 // 所有的HTTP请求，就都可以交给了该实例处理了
 type Engine struct {
-	router map[string]HandlerFunc //路由映射表
+	router *router //路由映射表
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
@@ -36,12 +34,8 @@ func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
 
 // Run defines the method to start a http server
